e85af54be51b9b3e182d7d26b409baa67c954c14
Stefan Jucker <stefan.jucker@gmail.com>
2024-12-01 10:18:36 +0100

    Initial commit

---
+++ src/main/java/org/vaadin/example/Application.java
@@ -0,0 +1,23 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ * <p>
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ */
+@SpringBootApplication
+@PWA(name = "Musikverein Harmonie Urdorf Platform", shortName = "MVU Platform")
+@Theme("my-theme")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
