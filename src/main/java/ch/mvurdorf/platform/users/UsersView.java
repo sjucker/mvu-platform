b63544666d821a2e650c835bcc038b906599fcc7
Stefan Jucker <stefan.jucker@gmail.com>
2025-09-10 21:51:12 +0200

    Add active user filtering in `UsersView` with checkbox and data filtering logic

--- src/main/java/ch/mvurdorf/platform/users/UsersView.java
+++ src/main/java/ch/mvurdorf/platform/users/UsersView.java
@@ -3,12 +3,15 @@ package ch.mvurdorf.platform.users;
 import ch.mvurdorf.platform.security.AuthenticatedUser;
 import ch.mvurdorf.platform.ui.LocalizedEnumRenderer;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.renderer.ComponentRenderer;
+import com.vaadin.flow.function.SerializablePredicate;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -35,6 +38,8 @@ class UsersView extends VerticalLayout {
 
     private HorizontalLayout controls;
     private Grid<UserDto> grid;
+    private GridListDataView<UserDto> dataView;
+    private Checkbox activeUsersOnly;
 
     public UsersView(UsersService usersService, AuthenticatedUser authenticatedUser) {
         this.usersService = usersService;
@@ -48,24 +53,31 @@ class UsersView extends VerticalLayout {
 
     private void createControls() {
         controls = new HorizontalLayout();
+        controls.setAlignItems(Alignment.CENTER);
+        controls.setJustifyContentMode(JustifyContentMode.BETWEEN);
+        controls.setWidthFull();
+
         if (authenticatedUser.hasWritePermission(USERS_GROUP)) {
             controls.add(new Button("Neuer User",
                                     _ -> UserDialog.create(newUser -> usersService.create(newUser)
                                                                                   .ifPresentOrElse(
                                                                                           password -> {
                                                                                               Notification.show("User erstellt mit Passwort: " + password, 0, TOP_CENTER);
-                                                                                              grid.setItems(usersService.findAll());
+                                                                                              setGridItems();
                                                                                           },
                                                                                           () -> Notification.show("User konnte nicht erstellt werden.")))));
         }
+
+        activeUsersOnly = new Checkbox("nur Aktive");
+        activeUsersOnly.setValue(true);
+        activeUsersOnly.addValueChangeListener(_ -> dataView.setFilter(dataFilter()));
+        controls.add(activeUsersOnly);
     }
 
     private void createGrid() {
         grid = new Grid<>();
         grid.addThemeVariants(LUMO_COLUMN_BORDERS, LUMO_COMPACT);
         grid.setHeightFull();
-
-        grid.setItems(usersService.findAll());
 
         grid.addColumn(clickableIcon(EDIT, this::edit)).setWidth("60px").setTextAlign(CENTER).setFlexGrow(0);
         grid.addColumn(UserDto::email).setHeader("Email");
@@ -74,12 +86,23 @@ class UsersView extends VerticalLayout {
         grid.addColumn(new ComponentRenderer<>(userDto -> userDto.active() ? CHECK.create() : new Div())).setHeader("Aktiv").setWidth("60px").setTextAlign(CENTER).setFlexGrow(0);
 
         grid.addItemDoubleClickListener(event -> edit(event.getItem()));
+
+        setGridItems();
+    }
+
+    private void setGridItems() {
+        dataView = grid.setItems(usersService.findAll());
+        dataView.setFilter(dataFilter());
     }
 
     private void edit(UserDto item) {
         UserDialog.edit(item, user -> {
             usersService.update(user);
-            grid.setItems(usersService.findAll());
+            setGridItems();
         });
+    }
+
+    private SerializablePredicate<UserDto> dataFilter() {
+        return dto -> !activeUsersOnly.getValue() || dto.active();
     }
 }
