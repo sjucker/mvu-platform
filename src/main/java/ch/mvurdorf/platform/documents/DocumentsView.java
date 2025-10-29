6a6d66af580c85f5926e07cdf78615fd344b6e29
Stefan Jucker <stefan.jucker@gmail.com>
2025-10-29 20:27:43 +0100

    MVU-Statuten (2025-03-13)

--- src/main/java/ch/mvurdorf/platform/documents/DocumentsView.java
+++ src/main/java/ch/mvurdorf/platform/documents/DocumentsView.java
@@ -3,18 +3,16 @@ package ch.mvurdorf.platform.documents;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.ListItem;
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.streams.DownloadHandler;
+import com.vaadin.flow.server.streams.DownloadResponse;
 import jakarta.annotation.security.PermitAll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
 
 import static com.vaadin.flow.component.html.AttachmentType.DOWNLOAD;
 import static org.vaadin.lineawesome.LineAwesomeIconUrl.FILE_ALT;
@@ -32,16 +30,11 @@ public class DocumentsView extends VerticalLayout {
     }
 
     private void create() {
-        add(new UnorderedList(new ListItem(downloadAnchor("documents/mvu-statuten.pdf", "MVU-Statuten (Stand 13. März 2025)"))));
+        add(new UnorderedList(new ListItem(downloadAnchor("documents/mvu-statuten.pdf", "mvu-statuten.pdf", "MVU-Statuten (Stand 13. März 2025)"))));
     }
 
-    private Component downloadAnchor(String path, String text) {
-        try {
-            return new Anchor(DownloadHandler.forFile(new ClassPathResource(path).getFile()), DOWNLOAD, text);
-        } catch (IOException e) {
-            log.error("could not create anchor", e);
-            return new Span("Ein Fehler ist aufgetreten...");
-        }
+    private Component downloadAnchor(String path, String filename, String text) {
+        return new Anchor(DownloadHandler.fromInputStream(_ -> new DownloadResponse(new ClassPathResource(path).getInputStream(), filename, null, -1)), DOWNLOAD, text);
     }
 
 }
