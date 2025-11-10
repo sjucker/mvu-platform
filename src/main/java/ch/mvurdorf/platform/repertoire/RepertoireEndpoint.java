7db5671f5619d482280d79f1c29db2d008bef8b0
Stefan Jucker <stefan.jucker@gmail.com>
2025-11-10 12:43:10 +0100

    Remove unused JWT

--- src/main/java/ch/mvurdorf/platform/repertoire/RepertoireEndpoint.java
+++ src/main/java/ch/mvurdorf/platform/repertoire/RepertoireEndpoint.java
@@ -3,8 +3,6 @@ package ch.mvurdorf.platform.repertoire;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +17,7 @@ public class RepertoireEndpoint {
     private final RepertoireService repertoireService;
 
     @GetMapping("/{type}")
-    public ResponseEntity<RepertoireDto> ping(@PathVariable RepertoireType type,
-                                              @AuthenticationPrincipal Jwt jwt) {
+    public ResponseEntity<RepertoireDto> ping(@PathVariable RepertoireType type) {
         log.info("GET /api/secured/repertoire/{}", type);
         return ResponseEntity.of(repertoireService.findRepertoireByType(type));
     }
