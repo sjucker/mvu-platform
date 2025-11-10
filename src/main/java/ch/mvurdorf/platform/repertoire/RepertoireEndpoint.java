29e9ea1dcd0e6eee9ab8607e1b5278c86d14d549
Stefan Jucker <stefan.jucker@gmail.com>
2025-11-10 12:41:40 +0100

    Implement RepertoireEndpoint

--- src/main/java/ch/mvurdorf/platform/repertoire/RepertoireEndpoint.java
+++ src/main/java/ch/mvurdorf/platform/repertoire/RepertoireEndpoint.java
@@ -1,21 +1,27 @@
 package ch.mvurdorf.platform.repertoire;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/secured/repertoire")
 public class RepertoireEndpoint {
 
-    @GetMapping
-    public ResponseEntity<String> ping(@AuthenticationPrincipal Jwt jwt) {
-        log.info("GET /api/secured/repertoire");
-        return ResponseEntity.ok("pong");
+    private final RepertoireService repertoireService;
+
+    @GetMapping("/{type}")
+    public ResponseEntity<RepertoireDto> ping(@PathVariable RepertoireType type,
+                                              @AuthenticationPrincipal Jwt jwt) {
+        log.info("GET /api/secured/repertoire/{}", type);
+        return ResponseEntity.of(repertoireService.findRepertoireByType(type));
     }
 }
