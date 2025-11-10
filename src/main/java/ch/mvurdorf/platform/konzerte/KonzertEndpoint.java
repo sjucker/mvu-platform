a78443176c5c18ac3e6c4acf039335dd7337e6f2
Stefan Jucker <stefan.jucker@gmail.com>
2025-11-10 12:48:08 +0100

    Implement KonzertEndpoint

---
+++ src/main/java/ch/mvurdorf/platform/konzerte/KonzertEndpoint.java
@@ -0,0 +1,33 @@
+package ch.mvurdorf.platform.konzerte;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/secured/konzert")
+public class KonzertEndpoint {
+
+    private final KonzerteService konzerteService;
+
+    @GetMapping
+    public ResponseEntity<List<KonzertDto>> ping() {
+        log.info("GET /api/secured/konzert");
+        return ResponseEntity.ok(konzerteService.getFutureKonzerte());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<KonzertDto> ping(@PathVariable Long id) {
+        log.info("GET /api/secured/konzert/{}", id);
+        return ResponseEntity.of(konzerteService.findById(id));
+    }
+
+}
