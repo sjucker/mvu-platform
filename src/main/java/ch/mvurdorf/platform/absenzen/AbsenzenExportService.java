eb3e2cda88862f11a4de037735d58d157de715ce
Stefan Jucker <stefan.jucker@gmail.com>
2026-01-15 20:56:39 +0100

    Add export functionality to `AbsenzenView`

---
+++ src/main/java/ch/mvurdorf/platform/absenzen/AbsenzenExportService.java
@@ -0,0 +1,120 @@
+package ch.mvurdorf.platform.absenzen;
+
+import ch.mvurdorf.platform.common.AbsenzState;
+import ch.mvurdorf.platform.jooq.tables.records.AbsenzStatusRecord;
+import ch.mvurdorf.platform.jooq.tables.records.EventRecord;
+import ch.mvurdorf.platform.jooq.tables.records.LoginRecord;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.map.HashedMap;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static ch.mvurdorf.platform.common.AbsenzState.POSITIVE;
+import static ch.mvurdorf.platform.common.AbsenzState.UNKNOWN;
+import static ch.mvurdorf.platform.jooq.Tables.ABSENZ_STATUS;
+import static ch.mvurdorf.platform.jooq.Tables.EVENT;
+import static ch.mvurdorf.platform.jooq.Tables.LOGIN;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AbsenzenExportService {
+    private final DSLContext jooqDsl;
+
+    public InputStream export(LocalDate fromDate, LocalDate toDate) throws IOException {
+        var users = findActiveUsers();
+        var events = findEvents(fromDate, toDate);
+        var absenzen = findAbsenzenPerEventThenPerUser(users, events);
+
+        try (var workbook = new XSSFWorkbook()) {
+            var sheet = workbook.createSheet("Absenzen");
+            createHeader(sheet, events);
+            createRows(sheet, users, events, absenzen);
+
+            var stream = new ByteArrayOutputStream();
+            workbook.write(stream);
+            return new ByteArrayInputStream(stream.toByteArray());
+        }
+    }
+
+    private void createRows(XSSFSheet sheet, List<LoginRecord> users, List<EventRecord> events, Map<Long, Map<Long, AbsenzState>> absenzen) {
+        var rowNum = 1;
+        for (var user : users) {
+            var row = sheet.createRow(rowNum++);
+            var colNum = 0;
+            var cell = row.createCell(colNum++);
+            cell.setCellValue(user.getName());
+            var totalCell = row.createCell(colNum++);
+            var total = 0;
+            for (var event : events) {
+                var eventAbsenzen = absenzen.getOrDefault(event.getId(), new HashedMap<>());
+                cell = row.createCell(colNum++);
+                var absenzStatus = eventAbsenzen.getOrDefault(user.getId(), UNKNOWN);
+                cell.setCellValue(absenzStatus.getDescription());
+                if (absenzStatus == POSITIVE) {
+                    total++;
+                }
+            }
+            totalCell.setCellValue(total);
+        }
+    }
+
+    private void createHeader(XSSFSheet sheet, List<EventRecord> events) {
+        var headerRow = sheet.createRow(0);
+        var colNum = 0;
+        var cell = headerRow.createCell(colNum++);
+        cell.setCellValue("Name");
+        cell = headerRow.createCell(colNum++);
+        cell.setCellValue("Total");
+        for (var event : events) {
+            cell = headerRow.createCell(colNum++);
+            cell.setCellValue(event.getTitle());
+        }
+    }
+
+    private Map<Long, Map<Long, AbsenzState>> findAbsenzenPerEventThenPerUser(List<LoginRecord> users, List<EventRecord> events) {
+        var userIds = users.stream().map(LoginRecord::getId).toList();
+        var eventIds = events.stream().map(EventRecord::getId).toList();
+
+        return jooqDsl.selectFrom(ABSENZ_STATUS)
+                      .where(ABSENZ_STATUS.FK_EVENT.in(eventIds))
+                      .and(ABSENZ_STATUS.FK_LOGIN.in(userIds))
+                      .fetch()
+                      .stream()
+                      .collect(groupingBy(AbsenzStatusRecord::getFkEvent,
+                                          toMap(AbsenzStatusRecord::getFkLogin, it -> AbsenzState.of(it.getStatus()))));
+    }
+
+    private record Foo(Long loginId, Long eventId, String remark, String status) {}
+
+    private List<EventRecord> findEvents(LocalDate fromDate, LocalDate toDate) {
+        return jooqDsl.selectFrom(EVENT)
+                      .where(EVENT.NEXT_VERSION.isNull())
+                      .and(EVENT.DELETED_AT.isNull())
+                      .and(EVENT.FROM_DATE.between(fromDate, toDate))
+                      .and(EVENT.RELEVANT_FOR_ABSENZ.isTrue())
+                      .and(EVENT.INFO_ONLY.isFalse())
+                      .orderBy(EVENT.FROM_DATE.asc(), EVENT.FROM_TIME.asc(), EVENT.TO_DATE.asc(), EVENT.TO_TIME.asc())
+                      .fetch();
+    }
+
+    private List<LoginRecord> findActiveUsers() {
+        return jooqDsl.selectFrom(LOGIN)
+                      .where(LOGIN.ACTIVE.isTrue())
+                      .orderBy(LOGIN.NAME.asc())
+                      .fetch();
+    }
+}
--- src/main/java/ch/mvurdorf/platform/absenzen/AbsenzenView.java
+++ src/main/java/ch/mvurdorf/platform/absenzen/AbsenzenView.java
@@ -1,7 +1,11 @@
 package ch.mvurdorf.platform.absenzen;
 
 import ch.mvurdorf.platform.security.AuthenticatedUser;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -9,16 +13,27 @@ import com.vaadin.flow.data.provider.ConfigurableFilterDataProvider;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+import com.vaadin.flow.server.streams.DownloadResponse;
 import jakarta.annotation.security.RolesAllowed;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
 
 import static ch.mvurdorf.platform.security.LoginService.ABSENZEN_GROUP;
+import static ch.mvurdorf.platform.ui.ComponentUtil.datePicker;
 import static ch.mvurdorf.platform.ui.RendererUtil.clickableIcon;
 import static ch.mvurdorf.platform.ui.RendererUtil.dateRenderer;
+import static ch.mvurdorf.platform.utils.FormatUtil.formatDate;
 import static com.vaadin.flow.component.grid.ColumnTextAlign.CENTER;
 import static com.vaadin.flow.component.icon.VaadinIcon.CHECK_SQUARE_O;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.BASELINE;
+import static com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode.BETWEEN;
 import static com.vaadin.flow.data.value.ValueChangeMode.TIMEOUT;
+import static com.vaadin.flow.server.streams.DownloadHandler.fromInputStream;
 import static org.vaadin.lineawesome.LineAwesomeIconUrl.CLIPBOARD_CHECK_SOLID;
 
+@Slf4j
 @PageTitle("Absenzen")
 @Route("absenzen")
 @RolesAllowed({ABSENZEN_GROUP})
@@ -26,14 +41,21 @@ import static org.vaadin.lineawesome.LineAwesomeIconUrl.CLIPBOARD_CHECK_SOLID;
 public class AbsenzenView extends VerticalLayout {
 
     private final AbsenzenService absenzenService;
+    private final AbsenzenExportService absenzenExportService;
     private final AuthenticatedUser authenticatedUser;
 
     private HorizontalLayout controls;
     private Grid<EventAbsenzSummaryDto> grid;
     private ConfigurableFilterDataProvider<EventAbsenzSummaryDto, Void, String> dataProvider;
+    private DatePicker fromDate;
+    private DatePicker toDate;
+    private Button exportButton;
 
-    public AbsenzenView(AbsenzenService absenzenService, AuthenticatedUser authenticatedUser) {
+    public AbsenzenView(AbsenzenService absenzenService,
+                        AbsenzenExportService absenzenExportService,
+                        AuthenticatedUser authenticatedUser) {
         this.absenzenService = absenzenService;
+        this.absenzenExportService = absenzenExportService;
         this.authenticatedUser = authenticatedUser;
 
         setSizeFull();
@@ -65,7 +87,38 @@ public class AbsenzenView extends VerticalLayout {
         filter.setValueChangeMode(TIMEOUT);
         filter.setClearButtonVisible(true);
 
-        controls.add(filter);
+        fromDate = datePicker("Von");
+        toDate = datePicker("Bis");
+        exportButton = new Button("Export");
+        exportButton.setEnabled(false);
+        exportButton.addClickListener(_ -> {
+            var from = fromDate.getValue();
+            var to = toDate.getValue();
+            try (var inputStream = absenzenExportService.export(from, to)) {
+                var resource = fromInputStream(_ -> new DownloadResponse(inputStream, "absenzen-%s-%s.xlsx".formatted(formatDate(from), formatDate(to)), "application/xlsx", -1));
+                var registration = VaadinSession.getCurrent().getResourceRegistry().registerResource(resource);
+                UI.getCurrent().getPage().open(registration.getResourceUri().toString());
+            } catch (IOException e) {
+                log.error("could not export excel", e);
+                Notification.show("Ein Fehler ist aufgetreten.");
+            }
+        });
+
+        fromDate.addValueChangeListener(_ -> validateForm());
+        toDate.addValueChangeListener(_ -> validateForm());
+
+        var exportControls = new HorizontalLayout(fromDate, toDate, exportButton);
+        exportControls.setAlignItems(BASELINE);
+
+        controls.add(filter, exportControls);
+        controls.setJustifyContentMode(BETWEEN);
+        controls.setAlignItems(BASELINE);
+    }
+
+    private void validateForm() {
+        var from = fromDate.getValue();
+        var to = toDate.getValue();
+        exportButton.setEnabled(from != null && to != null && !from.isAfter(to));
     }
 
     private void detail(EventAbsenzSummaryDto item) {
